eda99fd860c3bb3c2361ef706bc506d00d459424
geonryulPark <geonryul0131@gmail.com>
2023-01-17 16:34:36 +0900

    feat(ryul):create folder

---
+++ back_projects/fastapi-book-2.02/frontend/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  let message;
+
+  fetch("http://127.0.0.1:8000/hello").then((response) => {
+    response.json().then((json) => {
+      message = json.message;
+    });
+  });
+</script>
+
+<h1>{message}</h1>
